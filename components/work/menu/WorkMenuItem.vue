d1465607874240c054e8ae668d89a146a1d96c65
SnooHD <snoohd@gmail.com>
2022-08-24 18:50:45 +0200

    add work menu item component

--- components/work/menu/WorkMenuItem.vue
+++ components/work/menu/WorkMenuItem.vue
@@ -1,9 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  intro: {
+    type: String,
+    required: false,
+    default: null
+  },
+  image: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
 <template>
   <li>
     <NuxtLink :to="to">
       <span>{{ title }}</span>
       <span v-if="intro">{{ intro }}</span>
-      <NuxtImage></NuxtImage>
+      <img :src="image" />
     </NuxtLink>
   </li>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   typescript: {
     shim: false
   },
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
